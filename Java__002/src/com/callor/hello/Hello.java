f451f8fb4c4c6f44a45f54b350a3f2ebb76c5dd9
gpqls12321 <hhb_3414@naver.com>
2021-02-02 14:32:43 +0900

    파일모으기

---
+++ Java__002/src/com/callor/hello/Hello.java
@@ -0,0 +1,15 @@
+package com.callor.hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("30 + 40 =" + (30 + 40));
+		System.out.println("30 * 40 =" + 30 * 40);
+		System.out.println("40 - 30 =" + (40 - 30));
+		System.out.println("40 / 20 =" + 40 / 20);
+		System.out.println("여러분 반갑습니다");
+
+	}
+
+}
---
+++ Java__002/src/module-info.java
@@ -0,0 +1,2 @@
+module Java__002 {
+}
