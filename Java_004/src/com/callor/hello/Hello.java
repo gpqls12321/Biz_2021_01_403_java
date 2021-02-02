684dd7712d606950f04e8cf10376fa1fd2417cae
gpqls12321 <hhb_3414@naver.com>
2021-02-02 16:43:08 +0900

    추가

--- Java_004/src/com/callor/hello/Hello.java
+++ Java_004/src/com/callor/hello/Hello.java
@@ -1,5 +1,20 @@
 package com.callor.hello;
 
 public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println(30 + 40);
+		System.out.println(30 * 40);
+		
+		int num1 = 58;
+		int num2 = 55;
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 * num2);
+		System.out.println(num2 - num1);
+		System.out.println(num2 / num1);
+		System.out.println(num1 % num2);
+		
+	}
 
 }
