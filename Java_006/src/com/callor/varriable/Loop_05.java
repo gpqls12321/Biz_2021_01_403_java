1da8fdba9b3d860afe31a0ecee7e3497ae237fbf
gpqls12321 <hhb_3414@naver.com>
2021-02-04 16:33:37 +0900

    반복문

---
+++ Java_006/src/com/callor/varriable/Loop_05.java
@@ -0,0 +1,33 @@
+package com.callor.varriable;
+
+public class Loop_05 {
+
+	public static void main(String[] args) {
+		
+		int intDan = 7;
+		for(int num = 1; num < 10 ; num += 1) {
+			
+		}
+		for(int num = 1; num < 10 ; num += 1) {
+	    // num 라는 변수가 1부터 1씩 증가하여 10미만 동안 9번 코드를 반복하는 명령문이 된다.
+		}
+		for(int num = 1; num < 100 ; num +=1) {
+		// 99번 코드를 반복
+		}
+		
+		for(int num = 20 ; num < 42 ; num +=1) {
+			System.out.println("안녕");
+		}
+		
+		//  시작하는값    비교하는값  증가하는값
+		for(int num = 12 ; num <= 20 ; num += 1) {
+			System.out.println("반가워");
+		}
+		
+		for(int num = 12 ; num <= 20 ; num += 2) {
+			System.out.println(num);
+		}
+
+	}
+
+}
