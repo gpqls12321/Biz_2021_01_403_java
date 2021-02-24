eecbc0498726fb81574bb4311688ec5cd67415a5
gpqls12321 <hhb_3414@naver.com>
2021-02-24 16:41:50 +0900

    ScoreVO

--- Java_016_ClassScore/src/com/callor/score/Score_07.java
+++ Java_016_ClassScore/src/com/callor/score/Score_07.java
@@ -1,5 +1,14 @@
 package com.callor.score;
 
+import com.callor.score.service.model.ScoreVO;
+
 public class Score_07 {
+
+	public static void main(String[] args) {
+
+		ScoreVO scoreVO = new ScoreVO();
+		
+		scoreVO.
+	}
 
 }
