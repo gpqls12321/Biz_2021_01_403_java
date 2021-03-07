4cc382d804892d72233c74287b41619077a30e6c
gpqls12321 <hhb_3414@naver.com>
2021-03-07 22:30:44 +0900

    homework

--- Java_023_ScoreManage/src/com/callor/score/service/impl/MenuServiceImplV1.java
+++ Java_023_ScoreManage/src/com/callor/score/service/impl/MenuServiceImplV1.java
@@ -14,17 +14,38 @@ public class MenuServiceImplV1 implements MenuService {
 	
 	public Integer selectMenu() {
 		
-		System.out.println(Values.dLine);
-		System.out.println("빛나라 고교 성적 처리 시스템");
-		System.out.println(Values.sLine);
-		System.out.println(Values.SCORE_INPUT + ". 학생성적 점수 생성");
-		System.out.println(Values.SCORE_SAVE + ". 학생성적 점수 파일에 저장");
-		System.out.println(Values.SCORE_CHECK + ". 성적결과 확인");
-		System.out.println("QUIT. 끝내기");
-		System.out.println(Values.dLine);
-		System.out.print("선택 >> ");
+		while(true) {
+			System.out.println(Values.dLine);
+			System.out.println("빛나라 고교 성적 처리 시스템");
+			System.out.println(Values.sLine);
+			System.out.println(Values.SCORE_INPUT + ". 학생성적 점수 생성");
+			System.out.println(Values.SCORE_SAVE + ". 학생성적 점수 파일에 저장");
+			System.out.println(Values.SCORE_CHECK + ". 성적결과 확인");
+			System.out.println("QUIT. 끝내기");
+			System.out.println(Values.dLine);
+			System.out.print("선택 >> ");
+			String strMenu = scan.nextLine();
+			
+			if (strMenu.equals("QUIT")) {
+				return null;
+			}
+			
+			Integer intMenu = null;
+			try {
+				intMenu = Integer.valueOf(strMenu);
+			} catch (Exception e) {
+				System.out.printf("메뉴는 QUIT, %d ~ %d 까지만 선택 가능", Values.MENU_START, Values.MENU_LAST);
+				continue;
+			}
+			
+			if(intMenu >= Values.MENU_START && intMenu <= Values.MENU_LAST) {
+				return intMenu;
+			} else {
+				System.out.printf("업무는 %d ~ %d 중에 선택",Values.MENU_START, Values.MENU_LAST);
+			}
+			
+		}
 		
-		return null;
 	}
 
 }
--- Java_023_ScoreManage/src/com/callor/score/service/impl/ScoreServiceImplV1.java
+++ Java_023_ScoreManage/src/com/callor/score/service/impl/ScoreServiceImplV1.java
@@ -1,24 +1,34 @@
 package com.callor.score.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import com.callor.score.service.ScoreService;
 
 public class ScoreServiceImplV1 implements ScoreService {
 
+	private List<Integer> intList;
+	public ScoreServiceImplV1() {
+		intList = new ArrayList<Integer>();
+	}
+	
 	@Override
 	public void makeScore() {
-		// TODO Auto-generated method stub
-		
+		Random rnd = new Random();
+		for(int i = 0; i < 100; i++) {
+			Integer num = rnd.nextInt(100) + 1;
+			intList.add(num);
+		}
 	}
 
 	@Override
 	public void saveScoreToFile() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void loadScoreFromFile() {
-		// TODO Auto-generated method stub
 		
 	}
 
